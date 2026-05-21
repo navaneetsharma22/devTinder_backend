d932b28d6e624a1e01cdeb5479f435662c3e84e7
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-05-21 22:50:07 +0530

    feat: update package.json scripts and add /dashboard route in app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ app.use("/test", (req, res) => {
     res.send("Hello World");
 });
 
+app.use("/dashboard", (req, res) => {
+    res.send("Namste from dashboard")
+});
+
+
 app.listen(5000, () =>{
     console.log("Server is running on port 5000");
 });
