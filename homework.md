264672a6a2c7c9098e0bc49cdb58db749b3ed414
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-05-22 00:05:16 +0530

    feat: add request handlers for /hello and /hello/2 routes, and create homework.md file

---
+++ homework.md
@@ -0,0 +1,22 @@
+-create a repo
+-Initialize the repo 
+-node_modules,package.json, package-lock.json
+-Install express
+-create a  server
+-Listen to Port 5000
+-Write request handler for /test,/hello
+-Install nodemone and update script inside package.json
+-what are dependencies 
+- what is the use  of "-g" while npm install
+-Difference between caret and tilde (^ vs ~)
+
+
+lec -- 17 (ROuting and request Handling )
+-Initialize git 
+-.gitignore
+-Create a remote repo on github 
+-push all code to remote origin 
+-play with and route extension ex. /hello ,/, hello/2 ,/xyz
+-----Order Of route matter a lot 
+
+
--- src/app.js
+++ src/app.js
@@ -2,6 +2,24 @@ const express = require("express");
 
 const app = express();
 
+
+
+// app.use("/", (req, res) => {
+//     res.send("Overwrite the default route");
+// });
+
+app.use("/hello", (req, res) => {
+    res.send("hello the default route");
+});// not print /hello/2 because the first route is matched and it will not check for the second route
+
+app.use("/hello/2", (req, res) => {
+    res.send("abra ka dabra the route");
+});
+
+// app.use("/hello", (req, res) => {
+//     res.send("hello the default route");
+// });
+
 app.use("/test", (req, res) => {
     res.send("Hello World");
 });
@@ -9,6 +27,10 @@ app.use("/test", (req, res) => {
 app.use("/dashboard", (req, res) => {
     res.send("Namste from dashboard")
 });
+
+app.use("/", (req, res) => {
+    res.send("Overwrite the default route");
+});//
 
 
 app.listen(5000, () =>{
