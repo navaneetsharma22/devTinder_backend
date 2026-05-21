36e0fbc6f02916295b00eac37d4ae65418d624a6
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-05-21 22:49:54 +0530

    fix: correct route definition for /test endpoint in app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.use((req,res) =>{
+app.use("/test", (req, res) => {
     res.send("Hello World");
-}) ;
-
-
+});
 
 app.listen(5000, () =>{
     console.log("Server is running on port 5000");
