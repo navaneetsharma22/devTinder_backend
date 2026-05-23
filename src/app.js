67843cd1d9871ccbb5958caa2ebf8d155bc0e031
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-05-23 23:00:48 +0530

    refactor: comment out unused route handlers and clean up user endpoints

--- src/app.js
+++ src/app.js
@@ -8,29 +8,53 @@ const app = express();
 //     res.send("Overwrite the default route");
 // });
 
-app.use("/hello", (req, res) => {
-    res.send("hello the default route");
-});// not print /hello/2 because the first route is matched and it will not check for the second route
+// app.use("/hello", (req, res) => {
+//     res.send("hello the default route");
+// });// not print /hello/2 because the first route is matched and it will not check for the second route
 
-app.use("/hello/2", (req, res) => {
-    res.send("abra ka dabra the route");
-});
+// app.use("/hello/2", (req, res) => {
+//     res.send("abra ka dabra the route");
+// });
 
 // app.use("/hello", (req, res) => {
 //     res.send("hello the default route");
 // });
 
+app.get("/user", (req,res) =>{
+    res.send({
+        name: "John Doe",
+        lastname: "Doe",
+    })
+})
+
+app.post("/user",(req,res) => {
+    console.log("User created");
+    res.send("data successsfully saved to the database ");
+
+})
+
+app.delete("/user",(req,res) =>{
+    console.log("User deleted");
+    res.send("data successsfully deleted from the database ");
+})
+
+//Update user data 
+app.put("/user",(req,res) => {
+    console.log("User updated");
+    res.send("data successsfully updated in the database ");
+})
+
 app.use("/test", (req, res) => {
     res.send("Hello World");
 });
 
-app.use("/dashboard", (req, res) => {
-    res.send("Namste from dashboard")
-});
+// app.use("/dashboard", (req, res) => {
+//     res.send("Namste from dashboard")
+// });
 
-app.use("/", (req, res) => {
-    res.send("Overwrite the default route");
-});//
+// app.use("/", (req, res) => {
+// //     res.send("Overwrite the default route");
+// });//
 
 
 app.listen(5000, () =>{
