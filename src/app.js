f3c67a060b0a2c6267a0934a3cc8181fec077839
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-06-08 01:59:35 +0530

    feat: add PATCH API for user updates with error handling

--- src/app.js
+++ src/app.js
@@ -328,6 +328,23 @@ app.delete("/user", async (req,res) => {
     }
 })
 
+
+app.patch("/user" , async (req,res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+
+    try{
+        await User.findByIdAndUpdate({_id : userId},data);
+        
+        res.send("User updated successfully");
+
+    }catch(err) {
+        res.status(400).send("Something went wrong");
+    }
+
+
+})
+
 connectDB()
     .then(() => {
         console.log("Database Connected");
