6cbc77454d9affb09750506a34a6eaabdc7cd215
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-06-08 13:01:32 +0530

    feat: update user schema to enforce validation rules for lastName, emailid, age, and gender

--- src/models/user.js
+++ src/models/user.js
@@ -4,28 +4,45 @@ const userSchema = new mongoose.Schema({
     firstName : {
         type : String,
         required : true,
+        minlength : 3,
+        maxlength : 30,
+        trim : true, // it will remove the extra space from the string
     },
     lastName : {
         type : String,
-        
+        //required : true,
+        minlength : 3,
+        maxlength : 30,
+        trim : true, // it will remove the extra space from the string
     },
 
     emailid :{
         type : String,
         required : true,
         unique : true,
+        lowercase : true,
+        trim : true, 
 
     },
 
     age : {
         type: Number,
-    },
+        min:18,
+       
+        } ,
+     
 
     gender: {
         type: String,
+         validate (value) {
+            if(!["male", "female", "other"].includes(value)){
+                throw new Error("Gender must be male , female or other");
+            }
+        }
     },
     photoUrl : {
         type : String,
+        default : "https://cdn.vectorstock.com/i/500p/46/76/gray-male-head-placeholder-vector-23804676.jpg"
 
     },
     about : {
